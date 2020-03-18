5df501128ea05796eaa9b180a764b44f3f3df3df
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2020-03-18 14:32:52 +0100

    add my file

---
+++ ayoub essafi/this.js
@@ -0,0 +1,87 @@
+//this represent to the person object
+// var person = {
+//   firstName: "kim",
+//   lastName : "kan",
+//   fullName : function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+// console.log(person.fullName());
+
+
+
+//this refers to top level object
+// console.log(this);
+// // //window
+// console.log(this === window);
+// console.log(this.innerWidth);
+
+// "use strict";
+// function myFunction() {
+//   return this;
+// }
+// console.log(myFunction());
+
+// function animal() {
+//   this.name ="caty";
+//   this.say = function(){ 
+//     console.log(this);
+//   };
+// }
+// var anima = new animal();
+// anima.say();
+// console.log("hi my name is " + this.name)
+
+// function animal() {
+//   this.name ="caty";
+//   this.say = function(){ 
+//     console.log("hi my name is " + this.name)
+//     console.log(this);
+//   };
+// }
+// var animal = new animal();
+// (animal.say());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ayoub essafi/workshops.js
@@ -0,0 +1,45 @@
+/*
+//Comparaison de deux chaines de caractère en comptant le nombre de ses caractères  
+// function comp(chain1 , chain2){
+//     if(chain1.length > chain2.length){
+//         console.log(`${chain1} est supérieure de ${chain2}`)
+//     }else if(chain1.length === chain2.length){
+//         console.log(`${chain1} est égale a ${chain2}`)
+//     }else{
+//         console.log(`${chain1} est inférieure de ${chain2}`)
+//     }
+// }
+// comp("AB", "CDEF");
+// let a = "str";
+// let rev = a.split("").reverse().join("");
+// console.log(rev);
+*/
+
+
+/*
+// Récursivité et chaine de caractères 
+const reverse = (str) => {
+if(!str) {
+return str;
+}
+else{
+    return str.split("").reverse().join("");
+}
+}
+var z=reverse("aub");
+console.log(z);
+*/
+
+
+/*
+//convertir un nombre decimal de base de 10 en nombre binaire de Base de 2
+// function binaire() {
+//     var y = 120, x = "";
+//     while (y > 0) {
+//         x = (y % 2) + x;
+//         y = Math.floor(y / 2);
+//     }
+//     return x;
+// }
+// console.log(binaire());
+*/
