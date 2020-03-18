c689108cb859fe60b9e857d67f7ff21da19364cd
yahyabouhlal <yahya.bouhlal.b@gmail.com>
2020-03-18 13:48:51 +0100

    myWorkshop

---
+++ yahya's-js/Workshop2.js
@@ -0,0 +1,14 @@
+
+
+const reverse =(str) => {
+
+if(!str) {
+    return str;
+}
+else{
+    return str.split("").reverse().join("");
+}
+ }
+ var z= reverse("youssoufia");
+ console.log(z);
+
---
+++ yahya's-js/Workshop4.js
@@ -0,0 +1,10 @@
+const formating= (numbers) =>{
+
+    numbers.forEach(element => {
+         str = numbers.join("");
+    });
+    console.log(str);
+}
+
+formating([0,6,1,7,7,4,9,0,7,3]);
+
